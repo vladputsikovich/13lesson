4d62c44d4a01d41069468a18bd8443991707a5aa
vladputsikovich <vladputsikovich@gmail.com>
2021-12-05 00:20:39 +0300

    Add 13 lesson

--- 13Lesson/ViewController.swift
+++ 13Lesson/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    //MARK: Memory
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("viewDidLayoutSubviews")
+    }
+    //MARK: Oriemtation
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        super.willRotate(to: toInterfaceOrientation, duration: duration)
+        print("willRotate")
+    }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        super.didRotate(from: fromInterfaceOrientation)
+        print("didRotate")
+    }
 }
 
